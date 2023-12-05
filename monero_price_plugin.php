a159784951c41dd64b712bc12892ef7f002ce8f5
Brandywineo <152892618+Brandywineo@users.noreply.github.com>
2023-12-05 11:13:46 +0300

    Update monero_price_plugin.php

--- monero_price_plugin.php
+++ monero_price_plugin.php
@@ -28,7 +28,7 @@ function fetch_monero_price() {
 function write_monero_price_to_database($currency, $rate) {
     global $wpdb;
 
-    $table_name = $wpdb->prefix . 'monero_gateway_live_rates';
+    $table_name = $wpdb->prefix . 'moneron_gateway_live_rates';
 
     $query = $wpdb->prepare("INSERT INTO $table_name (currency, rate, updated) VALUES (%s, %d, NOW()) ON DUPLICATE KEY UPDATE rate=%d, updated=NOW()", array($currency, $rate, $rate));
 
@@ -43,15 +43,15 @@ function write_monero_price_to_database($currency, $rate) {
 // Shortcode to display Monero price
 function monero_price_shortcode() {
     $price = fetch_monero_price();
-    return "<p>Current Monero Price: \${$price}</p>";
+    return "<p id='monero-price'>Current Monero Price: \${$price}</p>";
 }
 
 add_shortcode('monero_price', 'monero_price_shortcode');
 
-// Schedule the task to run every 5 minutes
+// Schedule the task to run every 15 seconds
 function schedule_monero_price_update() {
     if (!wp_next_scheduled('update_monero_price')) {
-        wp_schedule_event(time(), 'five_minutes', 'update_monero_price');
+        wp_schedule_event(time(), '15_seconds', 'update_monero_price');
     }
 }
 
@@ -60,14 +60,48 @@ add_action('wp', 'schedule_monero_price_update');
 // Hook to execute the task
 add_action('update_monero_price', 'fetch_monero_price');
 
-// Define the interval
-function add_five_minutes_interval($schedules) {
-    $schedules['five_minutes'] = array(
-        'interval' => 5 * 60,
-        'display'  => __('Every 5 Minutes'),
+// Define the interval for 15 seconds
+function add_fifteen_seconds_interval($schedules) {
+    $schedules['15_seconds'] = array(
+        'interval' => 15,
+        'display'  => __('Every 15 Seconds'),
     );
 
     return $schedules;
 }
 
-add_filter('cron_schedules', 'add_five_minutes_interval');
+add_filter('cron_schedules', 'add_fifteen_seconds_interval');
+
+// Enqueue script for AJAX
+function monero_price_ajax_script() {
+    ?>
+    <script>
+        jQuery(document).ready(function($) {
+            function updateMoneroPrice() {
+                $.ajax({
+                    url: '<?php echo admin_url('admin-ajax.php'); ?>',
+                    type: 'POST',
+                    data: {
+                        'action': 'update_monero_price_ajax'
+                    },
+                    success: function(response) {
+                        $('#monero-price').html(response);
+                    }
+                });
+            }
+
+            setInterval(updateMoneroPrice, 15000); // 15 seconds interval
+        });
+    </script>
+    <?php
+}
+
+add_action('wp_footer', 'monero_price_ajax_script');
+
+// AJAX handler for updating Monero price
+function update_monero_price_ajax_handler() {
+    echo fetch_monero_price();
+    exit();
+}
+
+add_action('wp_ajax_update_monero_price_ajax', 'update_monero_price_ajax_handler');
